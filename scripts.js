26672321488624cb81e73b74f945b44ec534dee9
Tess Jensen <tmarij4@gmail.com>
2018-10-21 12:32:30 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+let request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = () => {
+  let data = JSON.parse(this.response);
+  data.forEach(data => console.log(data.title));
+  }
+}
+
+// Send request
+request.send()
