f61e0ca8bd171b0218679f596f6ff02330747788
Tess Jensen <tmarij4@gmail.com>
2018-10-21 13:26:18 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ let request = new XMLHttpRequest();
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
-request.onload = () => {
+request.onload = (){
   let data = JSON.parse(this.response);
   data.forEach(movie => {
     const card = document.createElement('div');
